a654253d31434d4db89343bc3b5fc543a67444e3
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-05-27 08:54:17 -0300

    feat: add filter select

---
+++ src/components/Select.tsx
@@ -0,0 +1,63 @@
+import {
+  Select as SelectContainer,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../components/ui/select";
+
+import { IoIosArrowDown } from "react-icons/io";
+
+const arrContinent = [
+  {
+    value: "Africa",
+    option: "Africa",
+  },
+  {
+    value: "Americas",
+    option: "America",
+  },
+  {
+    value: "Asia",
+    option: "Asia",
+  },
+  {
+    value: "Europe",
+    option: "Europe",
+  },
+  {
+    value: "Oceania",
+    option: "Oceania",
+  },
+  {
+    value: "All",
+    option: "All",
+  },
+];
+
+export default function Select() {
+  return (
+    <div className="relative w-[220px] shadow-around rounded">
+      <SelectContainer>
+        <SelectTrigger className="w-full outline-none rounded py-7 text-left pl-6 text-very_dark_blue text-sm font-semibold border-none h-14 bg-white">
+          <SelectValue placeholder="Filter by Region" />
+        </SelectTrigger>
+        <SelectContent className="border-none shadow-around rounded">
+          <SelectGroup>
+            {arrContinent.map((continent, index) => (
+              <SelectItem
+                key={index}
+                className="cursor-pointer"
+                value={continent.value}
+              >
+                {continent.option}
+              </SelectItem>
+            ))}
+          </SelectGroup>
+        </SelectContent>
+      </SelectContainer>
+      <IoIosArrowDown className="absolute top-2/4 -translate-y-2/4 right-3" />
+    </div>
+  );
+}
