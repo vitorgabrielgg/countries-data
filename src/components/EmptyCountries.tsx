ae88b2325b495b08619ef2fb723c0b2f334d9e0a
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-05-28 09:01:27 -0300

    feat: add component empty countries

---
+++ src/components/EmptyCountries.tsx
@@ -0,0 +1,7 @@
+export default function EmptyCountries() {
+  return (
+    <div className="text-center max-md:mt-6 px-5">
+      <p>Nenhum país encontrado, pesquise novamente.</p>
+    </div>
+  );
+}
--- src/layout/Countries.tsx
+++ src/layout/Countries.tsx
@@ -1,14 +1,15 @@
 import Container from "@/components/Container";
 import CountryCard from "@/components/CountryCard";
+import EmptyCountries from "@/components/EmptyCountries";
 import { useCountries } from "@/hooks/useCountries";
 
 export default function Countries() {
   const { countries } = useCountries();
   return (
     <section className="">
-      <Container className="grid grid-cols-auto_fit xl:grid-cols-4 justify-items-center px-5 gap-10 pb-20">
-        {countries &&
-          countries.map(
+      {countries.length > 0 ? (
+        <Container className="grid grid-cols-auto_fit xl:grid-cols-4 justify-items-center px-5 gap-10 pb-20">
+          {countries.map(
             ({ capital, flags, name, population, region, ...props }, index) => (
               <CountryCard
                 key={index}
@@ -21,7 +22,10 @@ export default function Countries() {
               />
             )
           )}
-      </Container>
+        </Container>
+      ) : (
+        <EmptyCountries />
+      )}
     </section>
   );
 }
