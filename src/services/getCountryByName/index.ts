f161762d79ef58172e5e5747542a9308f7115fbd
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-09-07 10:00:23 -0300

    feat: create service to get country by name

---
+++ src/services/getCountryByName/index.ts
@@ -0,0 +1,7 @@
+export const getCountryByName = async (name?: string) => {
+  const res = await fetch(
+    `https://restcountries.com/v3.1/name/${name}?fullText=true`
+  );
+  const json = await res.json();
+  return json;
+};
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,1 +1,2 @@
 export * from "./getAllCountries";
+export * from "./getCountryByName";
