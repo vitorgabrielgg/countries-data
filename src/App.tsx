00d5e5899be70e85c17da74a3854e5cf999ca9f2
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2024-09-07 09:21:27 -0300

    feat: create hook use query fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components";
 import { AppRoutes } from "./routes";
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <AppRoutes />
-    </>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/hooks/useQueryFetch/index.tsx
@@ -0,0 +1,17 @@
+import { CountryCard, CountryProps } from "@/@types";
+import { useQuery } from "@tanstack/react-query";
+
+type QueryMethod = (param?: string) => Promise<CountryCard[] | CountryProps[]>;
+
+export const useQueryFetch = (
+  method: QueryMethod,
+  queryKey: string,
+  parameter?: string
+) => {
+  const { data, isError, isLoading } = useQuery({
+    queryKey: [queryKey, parameter && parameter],
+    queryFn: () => method(parameter && parameter),
+  });
+
+  return [data, isLoading, isError];
+};
