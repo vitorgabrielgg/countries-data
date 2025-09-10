73306657a296ee93b7de83d703e8ca0c7f28d75e
Vitor Gabriel Gonçalves da Silva <vitorgabrielggds@gmail.com>
2025-09-10 10:19:59 -0300

    fix: adjust API address

--- src/services/getAllCountries/index.ts
+++ src/services/getAllCountries/index.ts
@@ -1,5 +1,7 @@
 export const getAllCountries = async () => {
-  const res = await fetch("https://restcountries.com/v3.1/all");
+  const res = await fetch(
+    "https://restcountries.com/v3.1/all?fields=name,flags,borders,cca3,population,capital,region,subregion,tld,languages"
+  );
   const json = await res.json();
   return json;
 };
